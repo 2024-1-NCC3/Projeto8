9cec6d4ced837b32610a03071f5c7da55c70eb76
VinnizzZ <vinnibinda@hotmail.com>
2024-05-22 21:28:23 -0300

    Atualizando o servidor

--- Backend/Server-node/db/db-connection.js
+++ Backend/Server-node/db/db-connection.js
@@ -1,4 +1,12 @@
 const mysql = require("mysql2");
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.listen(3306, () => {
+    console.log("Listening on port 3306")
+})
 
 var config = {
   host: "doesanguedb.mysql.database.azure.com",
@@ -17,5 +25,38 @@ conn.connect(function (err) {
     throw err;
   } else {
     console.log("Conexão estabelecida.");
+
+    app.post("/registrarUser", (req, res) => {
+      const novoUser = {
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: req.body.senha
+      }
+
+      conn.query('SELECT email FROM login WHERE email = ?', [novoUser.email],
+        function (err, results, fields) {
+            if(err){
+              throw err;
+            }
+            else if (result == null){
+              conn.query('INSERT INTO login (nome, email, senha) VALUES (?, ?, ?)', [novoUser.nome, novoUser.email, novoUser.senha],
+              function (err, results, fields) {
+                if (err) throw err;
+                else console.log('Foi inserido ' + results.affectedRows + ' linha(s)');
+              })
+
+              conn.end(function (err) {
+                if (err) throw err;
+                else  console.log('Done.')
+              });
+            }
+            else{
+              console.log("Usuario já inserido")
+            }
+
+        })
+
+    })
   }
 });
+
